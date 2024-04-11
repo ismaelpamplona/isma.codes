004bee66c65749d233d8a961b603c0e14d01def1
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-04-11 07:33:19 -0300

    (created) Issue #build_throttle_function.

---
+++ ripi/Issue/build_throttle_function/description.md
@@ -0,0 +1,1 @@
+# build_throttle_function (Issue)
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -17,3 +17,35 @@ export function fuzzySearch<T>(
   const fuse = new Fuse(all, options)
   return fuse.search(pattern)
 }
+
+export function throttle<T extends (...args: any[]) => any>(
+  fn: T,
+  waitTime: number
+): (...funcArgs: Parameters<T>) => void {
+  let timer: number | null = null
+  let lastExec: number | null = 0
+
+  const throttledFunction = function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
+    const context = this
+    const execFunction = () => {
+      fn.apply(context, args)
+      lastExec = Date.now()
+    }
+
+    if (!lastExec) {
+      execFunction()
+    } else {
+      clearTimeout(timer as number)
+      timer = window.setTimeout(
+        () => {
+          if (Date.now() - (lastExec as number) >= waitTime) {
+            execFunction()
+          }
+        },
+        waitTime - (Date.now() - (lastExec as number))
+      )
+    }
+  }
+
+  return throttledFunction as unknown as T
+}
