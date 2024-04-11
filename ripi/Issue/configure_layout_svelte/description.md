bc006f72c0028f2bc2df114707e9be5ad04cfc01
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-04-11 07:13:21 -0300

    (up) Issue #configure_layout_svelte.

--- ripi/Issue/configure_layout_svelte/description.md
+++ ripi/Issue/configure_layout_svelte/description.md
@@ -1,1 +1,5 @@
 # configure_layout_svelte (Issue)
+
+- [x] set up iconify
+- [x] import index.scss
+- [x] configure body container styles
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import 'iconify-icon'
+  import '../style/index.scss'
+</script>
+
+<div class="dark-theme">
+  Teste
+  <div class="body-container">
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  @import '../style/sizes.scss';
+
+  .body-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    min-height: calc(100vh - $nav-height);
+    padding: 0 20px;
+  }
+</style>
