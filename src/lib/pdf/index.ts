a079754f95acedc9c41bd2a556c5622462956399
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-04-15 21:27:06 -0300

    (up) Issue #build_pdf_api.

--- src/lib/pdf/index.ts
+++ src/lib/pdf/index.ts
@@ -2,24 +2,25 @@ import puppeteer from 'puppeteer-core'
 import chromium from '@sparticuz/chromium'
 import path from 'path'
 
-const currentDir = path.dirname(new URL(import.meta.url).pathname)
-
-const styleFilePath = path.resolve(currentDir, 'style-resume.css')
-
 export async function generatePdf(html: string) {
   const browser = await puppeteer.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath()
   })
 
+  const basePath = process.cwd()
+
+  const filePath = path.join(basePath, 'static/style-resume.css')
   const page = await browser.newPage()
 
   await page.setContent(html, {
     waitUntil: 'networkidle0'
   })
+
   await page.addStyleTag({
-    path: styleFilePath
+    path: filePath
   })
+
   const pdfBuffer = await page.pdf({ format: 'A4' })
 
   await browser.close()
